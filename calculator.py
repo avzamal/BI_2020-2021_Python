1ae4346353f18968beb4d3d5944c16f64f129685
Алексей Замалутдинов <Zamalutdinov@MacBook-Air-Aleksej.local>
2020-09-09 22:07:26 +0300

    I add calculator

---
+++ calculator.py
@@ -0,0 +1,29 @@
+# enter the 2 numbers and operation symbol and check first_num and second_num is numbers
+first_num = input()
+if not first_num.isnumeric():
+    print('First number is not a number')
+else:
+    first_num = int(first_num)
+action = input()
+second_num = input()
+if not second_num.isnumeric():
+    print('Second number is not a number')
+else:
+    second_num = int(second_num)
+# check the operation symbol and print the result
+if action == '-':
+    print(first_num - second_num)
+elif action == '+':
+    print(first_num + second_num)
+elif action == '*':
+    print(first_num * second_num)
+elif action == '/':
+    if second_num != 0: #check second_num is not zero
+        print(first_num / second_num)
+    else:
+        print('Division by zero')
+else:
+    print('Try another operation')
+
+
+
