a7d7f10c0d5057a178583152ee10c1b0984c5524
avzamal <70773075+avzamal@users.noreply.github.com>
2021-03-17 21:34:23 +0300

    Update Classes.py

--- Classes.py
+++ Classes.py
@@ -1,17 +1,17 @@
 class Dna:
-    
+
     def __init__(self, sequence):
         self.sequence = sequence.upper()
-        
+    
     def gc_content(self):
         total_gc = self.sequence.count('G') + self.sequence.count('C')
         return total_gc / len(self.sequence)
-    
+
     def reverse_complement(self):
-        translation_table = str.maketrans({'A':'T', 'T':'A', 'G':'C', 'C':'G'})
+        translation_table = str.maketrans({'A': 'T', 'T': 'A', 'G': 'C', 'C': 'G'})
         inverted_sequence = self.sequence[::-1]
         return inverted_sequence.translate(translation_table)
-    
+
     def __iter__(self):
         for each in self.sequence:
             yield each
@@ -20,29 +20,29 @@ class Dna:
         if isinstance(other, Dna):
             return self.sequence == other.sequence
         return NotImplemented
-    
+
     def __hash__(self):
         return hash(self.sequence)
-    
+
     def transcribe(self):
-        translation_table = str.maketrans({'T':'U'})
+        translation_table = str.maketrans({'T': 'U'})
         return Rna(self.sequence.translate(translation_table))
 
 
 class Rna:
-    
+
     def __init__(self, sequence):
         self.sequence = sequence.upper()
-        
+    
     def gc_content(self):
         total_gc = self.sequence.count('G') + self.sequence.count('C')
         return total_gc / len(self.sequence)
-    
+
     def reverse_complement(self):
-        translation_table = str.maketrans({'A':'U', 'U':'A', 'G':'C', 'C':'G'})
+        translation_table = str.maketrans({'A': 'U', 'U': 'A', 'G': 'C', 'C': 'G'})
         inverted_sequence = self.sequence[::-1]
         return inverted_sequence.translate(translation_table)
-    
+
     def __iter__(self):
         for each in self.sequence:
             yield each
@@ -51,6 +51,6 @@ class Rna:
         if isinstance(other, Dna):
             return self.sequence == other.sequence
         return NotImplemented
-    
+
     def __hash__(self):
         return hash(self.sequence)
